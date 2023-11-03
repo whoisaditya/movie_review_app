61ecbbb77ade041394ad402586663439aea437f1
whoisaditya <mitraaditya1357@gmail.com>
2023-11-04 04:03:18 +0530

    feat: add a route for get all movies under api/v1/movies

---
+++ src/main/java/com/movie_review_app/movie_review_app/MovieController.java
@@ -0,0 +1,22 @@
+package com.movie_review_app.movie_review_app;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/movie_review_app/movie_review_app/MovieService.java
@@ -0,0 +1,17 @@
+package com.movie_review_app.movie_review_app;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+    
+    @Autowired
+    private MovieRepository movieRepository;
+    
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+}
