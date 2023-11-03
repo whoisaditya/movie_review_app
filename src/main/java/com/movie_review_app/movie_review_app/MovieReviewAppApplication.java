e32e75a98845b3a4af16691723e11606df41a818
whoisaditya <mitraaditya1357@gmail.com>
2023-11-04 00:38:46 +0530

    feat: change application port and print hello world

--- src/main/java/com/movie_review_app/movie_review_app/MovieReviewAppApplication.java
+++ src/main/java/com/movie_review_app/movie_review_app/MovieReviewAppApplication.java
@@ -2,12 +2,19 @@ package com.movie_review_app.movie_review_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MovieReviewAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieReviewAppApplication.class, args);
 	}
-
+	
+	// @GetMapping("/root")
+	// public String apiRoot() {
+	// 	return "Hello, World!";
+	// }
 }
