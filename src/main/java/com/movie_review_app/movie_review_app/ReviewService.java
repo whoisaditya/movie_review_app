c4ca0bfec6f50e2029f4783ffeee818770437625
whoisaditya <mitraaditya1357@gmail.com>
2023-11-04 21:12:37 +0530

    fix: remove unecessary import

--- src/main/java/com/movie_review_app/movie_review_app/ReviewService.java
+++ src/main/java/com/movie_review_app/movie_review_app/ReviewService.java
@@ -7,7 +7,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.data.mongodb.core.query.UpdateDefinition;
 import org.springframework.stereotype.Service;
 
 @Service
