afd57eb29b63a8bc35b4db3114051301996a470a
whoisaditya <mitraaditya1357@gmail.com>
2023-11-04 17:17:12 +0530

    feat: add route post /api/v1/movies

---
+++ src/main/java/com/movie_review_app/movie_review_app/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.movie_review_app.movie_review_app;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+    
+}
