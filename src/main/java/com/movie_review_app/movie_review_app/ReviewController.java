88e2e6b1967aa3ce1033bb46d5d52df111d22cd0
whoisaditya <mitraaditya1357@gmail.com>
2023-11-04 20:09:03 +0530

    feat: add a route for updating reviews

--- src/main/java/com/movie_review_app/movie_review_app/ReviewController.java
+++ src/main/java/com/movie_review_app/movie_review_app/ReviewController.java
@@ -5,7 +5,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,10 @@ public class ReviewController {
     @PostMapping
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
         return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    }
+
+    @PutMapping("{reviewId}")
+    public ResponseEntity<Review> updateReview(@RequestBody Map<String, String> payload, @PathVariable String reviewId) {
+        return new ResponseEntity<Review>(reviewService.updateReview(payload.get("reviewBody"), reviewId), HttpStatus.OK);
     }
 }
--- src/main/java/com/movie_review_app/movie_review_app/ReviewService.java
+++ src/main/java/com/movie_review_app/movie_review_app/ReviewService.java
@@ -1,8 +1,11 @@
 package com.movie_review_app.movie_review_app;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.data.mongodb.core.query.UpdateDefinition;
 import org.springframework.stereotype.Service;
@@ -24,5 +27,14 @@ public class ReviewService {
             .first();
 
         return review;
+    } 
+
+    public Review updateReview(String reviewBody, String reviewId) {
+        ObjectId id = new ObjectId(reviewId);
+        Query query = new Query().addCriteria(Criteria.where("_id").is(id));
+        Update updateDefinition = new Update().set("body", reviewBody);
+        FindAndModifyOptions options = new FindAndModifyOptions().returnNew(true).upsert(false);
+
+        return mongoTemplate.findAndModify(query, updateDefinition, options, Review.class);
     } 
 }
